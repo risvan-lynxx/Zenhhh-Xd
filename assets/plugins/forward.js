9301add2375bf1d2d8277ccdc9b4e9a6c5373742
Prince Rudh <87886877+princerudh@users.noreply.github.com>
2025-04-18 16:38:06 +0530

    Update forward.js ✓

--- assets/plugins/forward.js
+++ assets/plugins/forward.js
@@ -1,15 +1,22 @@
-const { command, parsedJid,isPrivate } = require("../../lib/");
+const { command, parsedJid, isPrivate } = require("../../lib/");
 
 command(
   {
     pattern: "fd",
     fromMe: isPrivate,
-    desc: "Forwards the replied Message",
+    desc: "Forwards the replied message",
     type: "Util",
   },
   async (message, match, m) => {
-    if(!m.quoted) return message.reply('Reply to something') 
-    let jids = parsedJid(match);
-    for (let i of jids) {
-      await message.forward(i, message.reply_message.message);
-    }   
+    if (!m.quoted) {
+      return message.reply("Reply to a message you want to forward.");
+    }
+
+    const jids = parsedJid(match);
+    const forwardedMessage = message.reply_message.message;
+
+    for (const jid of jids) {
+      await message.forward(jid, forwardedMessage);
+    }
+  }
+);
